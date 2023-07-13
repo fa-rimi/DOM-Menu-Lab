5aacaeee3e1dcc305aa34bc8512de7841f380a94
rimi <a.fatimaarimi@gmail.com>
2023-07-13 16:54:20 -0400

    finished tasks section 3

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ mainEl.appendChild(h1Element);
 
 // * ========= Task 1.3 =========
 // Add a class of flex-ctr to mainEl.
-mainEl.classList.add("flex-ctr")
+mainEl.classList.add("flex-ctr");
 
 // ** Progress Check **
 
@@ -37,7 +37,7 @@ console.log(topMenuEl);
 
 // * ========= Task 2.1 =========
 // Set the height topMenuEl element to be 100%.
-topMenuEl.style.height = '100%';
+topMenuEl.style.height = "100%";
 
 // * ========= Task 2.2 =========
 // Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
@@ -45,7 +45,40 @@ topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 
 // * ========= Task 2.3 =========
 // Add a class of flex-around to topMenuEl.
-topMenuEl.classList.add("flex-around")
+topMenuEl.classList.add("flex-around");
 
 // ** Progress Check **
 
+// * ========= Task 3.0 =========
+// Copy the following data structure to the top of script.js:
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+// * ========= Task 3.1 =========
+/** Iterate over the entire menuLinks array and for each "link" object:
+ *
+ * Create an <a>element.
+ *
+ * On the new element, add an href attribute with its value set to the
+ * href property of the "link" object
+ *
+ * Set the new element's content to the value of the text property of
+ * the "link" object.
+ *
+ * Append the new element to the topMenuEl element
+ *
+ * */
+
+for (let link of menuLinks) {
+  const menu = document.createElement("a");
+  menu.setAttribute("href", link.href);
+  menu.innerHTML = link.text;
+  topMenuEl.append(menu);
+}
+
+// *** DONE ***
