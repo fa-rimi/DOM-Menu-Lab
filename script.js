7d54cca793e79064240ef2715b5b36eea22bd888
rimi <a.fatimaarimi@gmail.com>
2023-07-13 16:24:43 -0400

    finished tasks section 2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log(mainEl);
  * like this: 'var(--main-bg)'
 
 **/
-mainEl.style.background = "var(--main-bg)";
+mainEl.style.backgroundColor = "var(--main-bg)";
 
 // * ========= Task 1.2 =========
 // Set the content of mainEl to <h1>SEI Rocks!</h1>.
@@ -27,6 +27,25 @@ mainEl.appendChild(h1Element);
 // * ========= Task 1.3 =========
 // Add a class of flex-ctr to mainEl.
 mainEl.classList.add("flex-ctr")
+
+// ** Progress Check **
+
+// * ========= Task 2.0 =========
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.querySelector("#top-menu");
+console.log(topMenuEl);
+
+// * ========= Task 2.1 =========
+// Set the height topMenuEl element to be 100%.
+topMenuEl.style.height = '100%';
+
+// * ========= Task 2.2 =========
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+// * ========= Task 2.3 =========
+// Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add("flex-around")
 
 // ** Progress Check **
 
